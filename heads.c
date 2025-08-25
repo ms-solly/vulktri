82cd488833b9cf26c51d2b070ab997b4e8516958
ms-solly <theworldofthoughtsyo@gmail.com>
2025-08-25 07:05:03 +0530

    flags first then unity header importtt

--- heads.c
+++ heads.c
@@ -8,13 +8,14 @@
 #include <vulkan/vulkan_core.h>
 #include <vulkan/vulkan_wayland.h>
 // window creation
-#define GLFW_INCLUDE_VULKAN
-#define GLFW_EXPOSE_NATIVE_WAYLAND
+// #define GLFW_INCLUDE_VULKAN
+// #define GLFW_EXPOSE_NATIVE_WAYLAND
 #include <GLFW/glfw3.h>
 #include <GLFW/glfw3native.h>
 // gui headers
 // & macros
-//flags 
+//flags
+/*
 #define NK_INCLUDE_FIXED_TYPES
 #define NK_INCLUDE_STANDARD_IO
 #define NK_INCLUDE_STANDARD_VARARGS
@@ -24,6 +25,6 @@
 #define NK_INCLUDE_DEFAULT_FONT
 #define NK_IMPLEMENTATION
 #define NK_GLFW_VULKAN_IMPLEMENTATION
-
+*/
 #include "ext/Nuklear/nuklear.h"
 #include "ext/Nuklear/demo/glfw_vulkan/nuklear_glfw_vulkan.h"
--- readme.md
+++ readme.md
@@ -1,5 +1,9 @@
 c99 vulkan triangle , without any abstraction in procedural style for easy to understand code
 
+
 ## keymaps : 
 
 - N : hide/unhide gui window
+
+## refs from which i learnt and also used  in this project
+https://en.wikipedia.org/wiki/Unity_build
